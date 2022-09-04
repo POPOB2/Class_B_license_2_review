5ac0f9708a107e3bf6c892284df50e55776c3c27
PB2 <nft192891@gmail.com>
2022-09-04 16:24:17 +0800

    修正del功能的function, 與 改寫錯誤註解

--- base.php
+++ base.php
@@ -38,17 +38,21 @@ class DB{
 // +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 // ---------------------------------------------------------------------------------------------------------------------------------------------------------
 // 總結 : 
-// 1. 有很多$id要換成$arg, 用CTRL+D快選上一次處理
-// 2. 刪除所有isset
-// 3. 將else的$sql.=內容改為 "WHERE `id`='$id'" ;
-// 4. return 的ftech全部 改為 fetch
+// 1. 去掉 ...
+// 2. 有很多$id要換成$arg, 用CTRL+D快選上一次處理
+// 3. 把$id[0] 的 [0]陣列刪除
+// 4. 刪除所有isset
+// 5. 將else的$sql.=內容改為 "WHERE `id`='$id'" ;
+// 6. return 的ftech全部 改為 fetch
 
-// public function all(...$arg){                // 更改 : all -> find , $id -> $arg
-    public function find(...$id){
+// public function all(...$arg){                // 更改 : all -> find , ...$arg -> $id
+    public function find($id){
         $sql="select * from $this->table";
         // if(isset($id[0])){                // 刪除
-            if(is_array($id[0])){                   // 更改 : $id -> $arg
-                foreach($id[0] as $key => $value){  // 更改 : $id -> $arg
+            // if(is_array($id[0])){                   // 更改 : $arg[0] -> $id, 去除[0]
+            if(is_array($id)){                   
+                // foreach($id[0] as $key => $value){  // 更改 : $arg[0] -> $id, 去除[0]
+                foreach($id as $key => $value){  
                     $tmp[]="`$key`='$value'";
                 }
                 $sql.=" WHERE ".join(" AND ", $tmp); // 注意空白一定要寫, 因為寫成SQL語法, 接在上方table之後
@@ -66,84 +70,52 @@ class DB{
 }
 // +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 // ---------------------------------------------------------------------------------------------------------------------------------------------------------
-    // 總結:
-    // 1.將(...)的...刪除
-    // 2.將查詢全部 改成 刪除
-    // 3.return的 查詢 改為 執行, 並把執行($sql) 後面的fetch等內容刪除
+// 總結:
+// 1. 將查詢全部 改成 刪除
+// 2. return的 查詢 改為 執行, 並把執行($sql) 後面的fetch等內容刪除
     
 // public function find(...$id){                //  更改 : find(...$id) -> del($id){
 public function del($id){                         
 //  $sql="select * from $this->table";          //  更改 : select * -> DELERE
     $sql="DELETE from $this->table";
     
-    if(is_array($id[0])){
-        foreach($id[0] as $key => $value){
+    if(is_array($id)){
+        foreach($id as $key => $value){
             $tmp[]="`$key`='$value'";
         }
-        $sql.="WHERE".join("AND",$tmp);
+        $sql.=" WHERE ".join(" AND ",$tmp);
     }else{
-        $sql.="WHERE `id`=`$id`";
+        $sql.=" WHERE `id`='$id'";
     }
     // return $this->pdo->query($sql)->fetch(PDO::FETCH_ASSOC);   // 更改 : query -> exec // 刪除 : ->fetch(PDO::FETCH_ASSOC)
     return $this->pdo->exec($sql);
 }
 // +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 // ---------------------------------------------------------------------------------------------------------------------------------------------------------
-// 方案一.
-// 複製del 把del改為save,  並把所有$id改為$array 
-public function save1($array){
-    // 新增ifelse判斷式後, 把foreach放進判斷的true區
-    if(isset($array['id'])){
-        //  並把foreach的$id[0]改成$array
-        //  foreach($id[0] as $key => $value){
-            foreach($array as $key => $value){
-                $tmp[]="`$key`='$value'";
-            }
-            //  新增這段 更新用的sql語法
-            $sql="UPDATE $this->table SET ".join(',',$tmp)." WHERE `id`='{$array['id']}'";
-        }else{
-            //  新增這段 新增用的sql語法
-            $sql="INSERT INTO $this->table (`".join("`,`",array_keys($array))."`)
-            values ('".join("','",$array)."')";
-        }
-        // if(is_array($array[0])){                   // 刪除
-        //     foreach($array[0] as $key => $value){  // 取這段放到forach的true區
-        //         $tmp[]="`$key`=`$value`";          // 取這段放到forach的true區
-        //     }                                      // 取這段放到forach的true區
-        //     $sql.="WHERE".join("AND",$tmp);        // 刪除
-        // }else{                                     // 刪除
-        //     $sql.="WHERE `id`=`$array`";           // 刪除
-        // }                                          // 刪除
 
-        return $this->pdo->exec($sql);
-    }
-// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-// ---------------------------------------------------------------------------------------------------------------------------------------------------------
-// 方案二.
 // 總結 : 
-// 0. 將function改為save($array)
 // 1. 刪除$sql
-// 2. 將if的is_array($)改為isset, 其isset的內容 改為 $array['id']
-// 3. 在該isset執行區域的 $id[0] 改為 $array
+// 2. 將if的is_array($id)改為isset($array['id'])
+// 3. 將foreach的 $id 改為 $array
 // 4. 再if和else的最後執行區域, 寫上更新 與 新增 的$sql=sql語法
 
 
 public function save($array){
-     // $sql="DELETE from $this->table";         // 刪除
-     // if(is_array($id[0])){                    // 更改 : is_array -> isset , ($id[0]) -> ($array['id'])
+     // $sql="DELETE from $this->table";      // 刪除
+     // if(is_array($id)){                    // 更改 : is_array($id) -> isset($array['id'])
         if(isset($array['id'])){
-         // foreach($id[0] as $key => $value){   // 更改 : $id[0] -> $array
+         // foreach($id as $key => $value){   // 更改 : $id -> $array
             foreach($array as $key => $value){
                 $tmp[]="`$key`='$value'";
             }
-         // $sql.="WHERE".join("AND",$tmp);      // 更改
-            $sql="UPDATE $this->table SET "
+         // $sql.="WHERE".join("AND",$tmp);      // 更改為以下內容
+            $sql=" UPDATE $this->table SET "
             .join(',',$tmp)
             ." WHERE `id`='{$array['id']}'";
         }else{
             
-         // $sql.="WHERE `id`=`$array`";         // 更改
-            $sql="insert into $this->table 
+         // $sql.="WHERE `id`=`$array`";         // 更改為以下內容
+            $sql=" INSERT INTO $this->table 
             (`".join("`,`",array_keys($array))."`)
             values('".join("','",$array)."')";
         }
@@ -154,7 +126,7 @@ public function save($array){
 // ---------------------------------------------------------------------------------------------------------------------------------------------------------
 // 總結 : 
 // 1. 再function math 的(...$arg)內 於前面 新增 $math,$col,
-// 2. 將$sql語法 查詢全部 改為 查詢$math($col)
+// 2. 將$sql語法 * 改為 $math($col)
 // 3. 將最後 return 的 fetchAll...; 改為 fectchColumn();
 
 // public function all(...$arg){            // 更改 : all -> math  // 新增 : (...$arg) -> ($math,$col,...$arg)
@@ -166,7 +138,7 @@ public function math($math,$col,...$arg){
             foreach($arg[0] as $key => $value){
                 $tmp[]="`$key`='$value'";
             }
-            $sql.="WHERE".join("AND",$tmp);
+            $sql.=" WHERE ".join(" AND ",$tmp);
         }else{
             $sql.=$arg[0];
         }
@@ -211,6 +183,7 @@ function dd($array){
 
 $Total=new DB('total'); // 計算拜訪總人數的資料表
 $User=new DB('user');
+$News=new DB('news');
 
 // 判斷有無session
 if(!isset($_SESSION['total'])){ // 若無
